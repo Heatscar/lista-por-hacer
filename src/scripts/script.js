abbd66dd40e1988fc15affa7424096909a2c52cd
Heatscar <hectormmgdth@gmail.com>
2018-09-21 16:29:54 -0500

    Se añadio script de fecha

---
+++ src/scripts/script.js
@@ -0,0 +1,12 @@
+function mueveReloj(){
+    var LaFecha=new Date();
+    var Mes=new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    var diasem=new Array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado');
+    var diasemana=LaFecha.getDay();
+    var FechaCompleta="";
+    var NumeroDeMes="";
+    NumeroDeMes=LaFecha.getMonth();
+    FechaCompleta=diasem[diasemana]+" "+LaFecha.getDate()+" de "+Mes[NumeroDeMes]+" de "+LaFecha.getFullYear()+" ";
+    document.getElementById('fecha').innerHTML=FechaCompleta;
+    setTimeout('mueveReloj()',1000)
+} 
